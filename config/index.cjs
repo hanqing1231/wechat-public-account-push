10a5bc3abd180af1f79c017cd918260b8f26bb9a
hanqing1231 <93855170+hanqing1231@users.noreply.github.com>
2023-02-05 11:28:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '王越',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o3oR_55_Sf8ofhLF8h0lRpEizC2k',
+      id: 'o3oR_561fpLHZOUmAPxTO9rix9jM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'oDh1aD2N-fup5s1IEDYFuysQx-Tbhy-u158dhjyw9qA',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
