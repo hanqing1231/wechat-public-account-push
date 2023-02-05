ec0c54d8a0b1f168b564bc9bff221eb9d571974a
hanqing1231 <93855170+hanqing1231@users.noreply.github.com>
2023-02-05 10:31:56 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o3oR_55_Sf8ofhLF8h0lRpEizC2k',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	pqD9H6Pwd086USCi0iyg7r04u2ChN7SUmIX_TtkS-3k',
+      useTemplateId: 'pqD9H6Pwd086USCi0iyg7r04u2ChN7SUmIX_TtkS-3k',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '01-29',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	pqD9H6Pwd086USCi0iyg7r04u2ChN7SUmIX_TtkS-3k	',
+  CALLBACK_TEMPLATE_ID: 'pqD9H6Pwd086USCi0iyg7r04u2ChN7SUmIX_TtkS-3k	',
 
   CALLBACK_USERS: [
     {
